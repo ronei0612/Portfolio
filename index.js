13ccc3adadd22902d2d0fe1a7a32137ebb69426f
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-09-06 19:21:11 -0300

    Teste

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
